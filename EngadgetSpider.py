179c2f9ba09c0c255c715568c86b9ebd91a96498
No-Energy <No_Energy@outlook.com>
2016-09-14 17:39:51 +0800

    just write need content and add download pic

--- EngadgetSpider.py
+++ EngadgetSpider.py
@@ -8,6 +8,14 @@ import os  # 系统库
 is_get_img = True
 
 
+def down_img(path, img_url):
+    pic_name = img_url[img_url.rindex('/') + 1:]
+    file_pic_name = path + pic_name
+    # os.path.join连接两个文件名地址，就比os.path.join("D:\","test.txt")结果是D:\test.txt,需要存在目录
+    urllib.request.urlretrieve(img_url, file_pic_name)
+    print('down %s.jpg successful' % pic_name)
+
+
 def _get_children_comment(children_index, children_data, comment_data_list):
     children_index += 1
     for post_data in children_data:
@@ -27,23 +35,23 @@ def _get_children_comment(children_index, children_data, comment_data_list):
     return
 
 
-def _get_full_content(title, get_url):
+def _get_full_content(get_url):
+    # 读取内容页
     content_page = urllib.request.urlopen(get_url)
     content_html = content_page.read().decode('utf-8')
 
-    # 输出正文
-    title_path = get_url.replace('http://cn.engadget.com', '')
-    path = os.getcwd() + title_path
-    if len(json_data['path']) > 0:
-        path = json_data['path']
-    # 检查是否存在路径，若不存在则建立路径
-    if not os.path.exists(path):
-        os.makedirs(path)
-    file_path = path + 'index.html'
-    f = open(file_path, 'w')
-    f.write(content_html)
-    f.close()
-    print('post ok')
+    # 正则获取标题
+    # re搜寻返回结果使用group读取，默认0是匹配原文，1是匹配第一个括号内结果，类推
+    title = re.search(r'<title>(.*)</title>', content_html).group(1)
+    print('# Text:%s Url:%s' % (title, get_url))
+
+    # get content by re
+    content = re.search(r'<div class="copy post-body(.*)<!-- /.post-body -->', content_html, re.DOTALL).group(0)
+
+    # get image url by re
+    image_url_list = []
+    if is_get_img:
+        image_url_list = re.findall(r'<img alt="" src="(.*[jpg|jpeg|gif|png|bmp])"', content)
 
     # 匹配获取页面的POST ID
     post_id = re.search(r'var postID = \'(.*)\';', content_html).group(1)
@@ -70,7 +78,7 @@ def _get_full_content(title, get_url):
     # 选择值是所有的items，使用lambda方法对item中的第二个值排序
     sort_post_id_list = sorted(post_id_list.items(), key=lambda d: d[1])
 
-    comment_data_list = []
+    comment_data_list = ['\n']
     # 按排序结果输出评论，由于排序结果返回已经将字典转化为列(数组)的结构，所以需要在数组值后取前一位
     for sort_post_id in sort_post_id_list:
         post_data = post_data_list[sort_post_id[0]]
@@ -85,17 +93,30 @@ def _get_full_content(title, get_url):
             # 第一级子评论
             _get_children_comment(1, post_data['children'], comment_data_list)
 
-    # 输出评论
-    f = open(file_path, 'a')
-    f.write(''.join(comment_data_list))
+    # output full content
+    # 输出内容
+    title_path = get_url.replace('http://cn.engadget.com', '')
+    path = os.getcwd() + title_path
+    if len(json_data['path']) > 0:
+        path = json_data['path']
+    # 检查是否存在路径，若不存在则建立路径
+    if not os.path.exists(path):
+        os.makedirs(path)
+    file_path = path + 'index.html'
+    # open with utf-8 coding if gbk can't convert special code
+    f = open(file_path, 'w', encoding='utf-8')
+    f.write(content + ''.join(comment_data_list))
     f.close()
-    print('comment ok')
+    print('ok')
 
+    for img_url in image_url_list:
+        down_img(path, img_url)
 
 if __name__=='__main__':
     try:
         json_file = open("config.json")
-        json_data = json.loads(json_file.read(), 'r')  # r只读，w可写，a追加
+        json_data = json.loads(json_file.read(), 'r')  # r只读;w可写,创建;a追加,创建;w+可读,可写,创建;a+可读,追加,创建
+        json_file.close()
     except IOError:
         print("config file error")
     pages = json_data['pages'] + 1
@@ -110,12 +131,7 @@ if __name__=='__main__':
         # 因为直接送入表达式也会默认生成pattern，且能提高效率，这边只使用一次，无必要
         # html_re = re.compile(htmlReg)
         # content_List = re.findall(htmlRe, html)
-        content_list = re.findall(r'<h2 class="h2" itemprop="headline">(.*?)</h2>', html)
-        for content_original in content_list:
-            # index = contentOriginal.rfind('/">') + 3
-            content = re.search(r'/">(.*)</a>', content_original).group(1)
-            content_url = re.search(r'href="(.*)">', content_original).group(1)
-            print('# Text:%s Url:%s' % (content, content_url))
-            _get_full_content(content, content_url)
+        content_url_list = re.findall(r'<a itemprop="url" href="(.*)">', html)
+        for content_url in content_url_list:
+            _get_full_content(content_url)
     print('all')
-
